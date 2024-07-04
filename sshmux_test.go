b86e6832c83c6880499720c41d781cc7aaa36aca
iBug <git@ibugone.com>
2024-07-04 21:25:50 +0800

    test: Tidy up code

--- sshmux_test.go
+++ sshmux_test.go
@@ -12,17 +12,8 @@ import (
 	"time"
 )
 
-func mustRemoveAll(path string) {
-	err := os.RemoveAll(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func mustGenerateKey(path string, type_ string) {
-	mustRemoveAll(path)
-
-	err := exec.Command("ssh-keygen", "-t", type_, "-f", path, "-N", "").Run()
+func mustGenerateKey(path string, typ string) {
+	err := exec.Command("ssh-keygen", "-t", typ, "-f", path, "-N", "").Run()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,7 +37,6 @@ func sshAPIHandler(w http.ResponseWriter, r *http.Request) {
 		Status:     "ok",
 		Address:    "127.0.0.1:2333",
 		Id:         1141919,
-		Cert:       "",
 		PrivateKey: examplePrivate,
 	}
 	jsonRes, err := json.Marshal(res)
@@ -66,13 +56,8 @@ func initHttp() {
 	}
 }
 
-func initEnv(t *testing.T) {
-	// Create /etc/sshmux/ssh_host_*_key
-	baseDir := "/tmp/sshmux"
-	err := os.MkdirAll(baseDir, 0o755)
-	if err != nil {
-		log.Fatal(err)
-	}
+func initEnv(t *testing.T, baseDir string) {
+	// Create host keys for sshd
 	mustGenerateKey(filepath.Join(baseDir, "ssh_host_rsa_key"), "rsa")
 	mustGenerateKey(filepath.Join(baseDir, "ssh_host_ecdsa_key"), "ecdsa")
 	mustGenerateKey(filepath.Join(baseDir, "ssh_host_ed25519_key"), "ed25519")
@@ -89,34 +74,66 @@ func initEnv(t *testing.T) {
 	go initHttp()
 }
 
-func startOnetimeSSHDServer() {
+func onetimeSSHDServer(t *testing.T, baseDir string) *exec.Cmd {
 	sshdPath, err := exec.LookPath("sshd")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
-	err = exec.Command(sshdPath, "-h", "/tmp/sshmux/ssh_host_ed25519_key", "-p", "2333", "-d", "-o", "AuthorizedKeysFile=/tmp/sshmux/example_rsa.pub", "-o", "StrictModes=no").Run()
-	if err != nil {
-		log.Println("sshd: ", err)
+	cmd := exec.Command(
+		sshdPath, "-d",
+		"-h", filepath.Join(baseDir, "ssh_host_ed25519_key"),
+		"-p", "2333",
+		"-o", "AuthorizedKeysFile="+filepath.Join(baseDir, "example_rsa.pub"),
+		"-o", "StrictModes=no")
+	cmd.Dir = baseDir
+	if err := cmd.Start(); err != nil {
+		t.Fatal("sshd: ", err)
+	}
+	return cmd
+}
+
+func waitForSSHD(t *testing.T, cmd *exec.Cmd) {
+	if err := cmd.Wait(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok && exitErr.ExitCode() == 255 {
+			// pass
+		} else {
+			t.Fatal("sshd: ", err)
+		}
 	}
 }
 
+func sshCommand(port, privateKeyPath string) *exec.Cmd {
+	return exec.Command(
+		"ssh", "-p", port,
+		"-o", "StrictHostKeyChecking=no",
+		"-o", "ControlMaster=no",
+		"-i", privateKeyPath,
+		"-o", "IdentityAgent=no",
+		"localhost", "uname")
+}
+
 func TestSSHClientConnection(t *testing.T) {
-	initEnv(t)
+	sleepDuration := 100 * time.Millisecond
+	baseDir := t.TempDir()
+
+	initEnv(t, baseDir)
+	privateKeyPath := filepath.Join(baseDir, "example_rsa")
 	go sshmuxServer("config.example.json")
 
 	// Sanity check
-	go startOnetimeSSHDServer()
-	time.Sleep(2 * time.Second)
-	err := exec.Command("ssh", "-p", "2333", "-o", "StrictHostKeyChecking=no", "-o", "ControlMaster=no", "-i", "/tmp/sshmux/example_rsa", "-o", "IdentityAgent=no", "localhost", "uname").Run()
+	cmd := onetimeSSHDServer(t, baseDir)
+	time.Sleep(sleepDuration)
+	err := sshCommand("2333", privateKeyPath).Run()
+	if err != nil {
+		t.Fatal("sanity check: ", err)
+	}
+	waitForSSHD(t, cmd)
+
+	cmd = onetimeSSHDServer(t, baseDir)
+	time.Sleep(sleepDuration)
+	err = sshCommand("8022", privateKeyPath).Run()
 	if err != nil {
 		t.Fatal("ssh: ", err)
 	}
-
-	time.Sleep(2 * time.Second)
-	go startOnetimeSSHDServer()
-	time.Sleep(2 * time.Second)
-	err = exec.Command("ssh", "-p", "8022", "-o", "StrictHostKeyChecking=no", "-o", "ControlMaster=no", "-i", "/tmp/sshmux/example_rsa", "-o", "IdentityAgent=no", "localhost", "uname").Run()
-	if err != nil {
-		t.Fatal("ssh2: ", err)
-	}
+	waitForSSHD(t, cmd)
 }
