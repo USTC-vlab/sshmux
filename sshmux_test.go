6ecdfcf903fcd11f72b5507512fbf6b0463bd388
YR Chen <stevapple@icloud.com>
2024-07-19 16:56:19 +0800

    Enable PROXY protocol in tests

--- sshmux_test.go
+++ sshmux_test.go
@@ -4,12 +4,16 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"syscall"
 	"testing"
 	"time"
+
+	"github.com/pires/go-proxyproto"
 )
 
 func mustGenerateKey(t *testing.T, path, typ string) {
@@ -35,7 +39,7 @@ func sshAPIHandler(w http.ResponseWriter, r *http.Request) {
 
 	res := &AuthResponse{
 		Status:     "ok",
-		Address:    "127.0.0.1:2333",
+		Address:    "127.0.0.1:2332",
 		Id:         1141919,
 		PrivateKey: examplePrivate,
 	}
@@ -54,6 +58,111 @@ func initHttp() {
 	if err := http.ListenAndServe("127.0.0.1:5000", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initUpstreamProxyServer() {
+	listener, err := net.Listen("tcp", "127.0.0.1:2222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+
+			go func() {
+				// 1. Set up downstream connection with sshmux
+				target, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8022")
+				if err != nil {
+					log.Fatal(err)
+				}
+				downstream, err := net.DialTCP("tcp", nil, target)
+				if err != nil {
+					log.Fatal(err)
+				}
+				// 2. Send PROXY header
+				header := proxyproto.HeaderProxyFromAddrs(1, conn.RemoteAddr(), downstream.RemoteAddr())
+				_, err = header.WriteTo(downstream)
+				if err != nil {
+					log.Fatal(err)
+				}
+				// 3. Forward TCP messages in a loop
+				for {
+					data := make([]byte, 0)
+					_, err := conn.Read(data)
+					if err != nil {
+						if err == syscall.EINVAL {
+							downstream.Close()
+						} else {
+							log.Print(err)
+						}
+						break
+					}
+					_, err = downstream.Write(data)
+					if err != nil {
+						log.Print(err)
+						break
+					}
+				}
+			}()
+		}
+	}()
+}
+
+func initDownstreamProxyServer() {
+	listener, err := net.Listen("tcp", "127.0.0.1:2232")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	proxyListener := &proxyproto.Listener{Listener: listener}
+	defer proxyListener.Close()
+
+	go func() {
+		for {
+			conn, err := proxyListener.Accept()
+			if err != nil {
+				log.Print(err)
+				continue
+			}
+
+			go func() {
+				// 1. Set up downstream connection with sshmux
+				target, err := net.ResolveTCPAddr("tcp", "127.0.0.1:2333")
+				if err != nil {
+					log.Fatal(err)
+				}
+				downstream, err := net.DialTCP("tcp", nil, target)
+				if err != nil {
+					log.Fatal(err)
+				}
+				// 2. Forward TCP messages in a loop
+				for {
+					data := make([]byte, 0)
+					_, err := conn.Read(data)
+					if err != nil {
+						if err == syscall.EINVAL {
+							downstream.Close()
+						} else {
+							log.Print(err)
+						}
+						break
+					}
+					_, err = downstream.Write(data)
+					if err != nil {
+						log.Print(err)
+						break
+					}
+				}
+			}()
+		}
+	}()
 }
 
 func initEnv(t *testing.T, baseDir string) {
@@ -83,6 +192,8 @@ func initEnv(t *testing.T, baseDir string) {
 
 	// Setup API Server
 	go initHttp()
+	go initUpstreamProxyServer()
+	go initDownstreamProxyServer()
 }
 
 func onetimeSSHDServer(t *testing.T, baseDir string) *exec.Cmd {
@@ -144,7 +255,7 @@ func TestSSHClientConnection(t *testing.T) {
 
 	cmd = onetimeSSHDServer(t, baseDir)
 	time.Sleep(sleepDuration)
-	err = sshCommand("8022", privateKeyPath).Run()
+	err = sshCommand("2222", privateKeyPath).Run()
 	if err != nil {
 		t.Fatal("ssh: ", err)
 	}
