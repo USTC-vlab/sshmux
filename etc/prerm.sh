b5d0cb6e4d4483621c6a2fd8d178e3b358a1d932
iBug <git@ibugone.com>
2024-08-17 10:05:15 +0800

    prerm.sh: Only disable service on removal

--- etc/prerm.sh
+++ etc/prerm.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-# Only run when systemd is running
-[ -d /run/systemd ] || exit 0
+case "$1" in
+  remove)
+    # Only run when systemd is running
+    [ -d /run/systemd ] || exit 0
 
-systemctl disable --now sshmux.service
+    systemctl disable --now sshmux.service
+    ;;
+  upgrade|deconfigure)
+    # Nothing to do
+    ;;
+  *)
+    echo "prerm script called with unknown argument: $1"
+    exit 1
+    ;;
+esac
